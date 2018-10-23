8a418942fd0096fd33ea9f979035b45a657b01e2
Hikiy <414950815@qq.com>
2018-10-23 13:39:48 +0800

    Dao层实现新增店铺、更新店铺功能,并单元测试

---
+++ src/main/java/hiki/o2o/dao/ShopDao.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package hiki.o2o.dao;
+
+import hiki.o2o.entity.Shop;
+
+/**
+ * @Hiki msi
+ *
+ */
+public interface ShopDao {
+	/**
+	 * 新增店铺
+	 * 
+	 * @param shop
+	 * @return 1插入一条，-1插入失败
+	 */
+	int insertShop(Shop shop);
+
+	/**
+	 * 更新店铺信息
+	 * 
+	 * @param shop
+	 * @return
+	 */
+	int updateShop(Shop shop);
+}
---
+++ src/test/java/hiki/o2o/dao/ShopDaoTest.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package hiki.o2o.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import hiki.o2o.BaseTest;
+import hiki.o2o.entity.Area;
+import hiki.o2o.entity.PersonInfo;
+import hiki.o2o.entity.Shop;
+import hiki.o2o.entity.ShopCategory;
+
+/**
+ * @Hiki msi
+ *
+ */
+public class ShopDaoTest extends BaseTest{
+	@Autowired
+	private ShopDao shopDao;
+	
+	@Test
+	@Ignore
+	public void testInsertShop(){
+		Shop shop=new Shop();
+		PersonInfo owner=new PersonInfo();
+		ShopCategory shopCategory=new ShopCategory();
+		Area area=new Area();
+		owner.setUserId(1L);
+		shopCategory.setShopCategoryId(1L);
+		area.setAreaId(1);
+		shop.setOwner(owner);
+		shop.setArea(area);
+		shop.setShopCategory(shopCategory);
+		shop.setShopName("测试的店铺");
+		shop.setShopDesc("test");
+		shop.setShopAddr("test");
+		shop.setPhone("test");
+		shop.setShopImg("test");
+		shop.setCreateTime(new Date());
+		shop.setEnableStatus(1);
+		shop.setAdvice("审核中");
+		int effectedNum=shopDao.insertShop(shop);
+		assertEquals(1,effectedNum);
+	}
+	@Test
+	public void testUpdateShop(){
+		Shop shop=new Shop();
+		shop.setShopId(2L);
+		shop.setShopDesc("测试描述");
+		shop.setShopAddr("测试地址");
+		shop.setLastEditTime(new Date());
+		int effectedNum=shopDao.updateShop(shop);
+		assertEquals(1,effectedNum);
+	}
+}
