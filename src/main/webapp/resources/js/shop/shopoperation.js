4fe65fa95ed65c561dc40e19ee9ce77b4b21af1e
Hikiy <414950815@qq.com>
2018-10-25 17:04:18 +0800

    用js实现后台获取店铺区域、店铺分类信息，并获取表单

---
+++ src/main/webapp/resources/js/shop/shopoperation.js
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+$(function() {
+	// 从后台获取店铺的基本信息（区域信息、店铺分类信息)，填充到两个控件：shop-category area
+	var initUrl = 'o2o/shop/getshopinitinfo'
+	var registerShopUrl = 'o2o/shopadmin/registershop';
+	alert(initUrl);
+	getShopInitInfo();
+	
+	function getShopInitInfo() {
+		// 第一个参数是访问的url，第二个是回调的方法
+		$.getJSON(initUrl, function(data) {
+			if (data.success) {
+				var tempHtml = '';
+				var tempAreaHtml = '';
+				// 遍历列表，保存option内容
+				data.shopCategotyList.map(function(item, index) {
+					tempHtml += '<option data-id="' + item.shopCategoryId
+							+ '">' + item.shopCategoryName + '</option>';
+				});
+				data.areaList.map(function(item, index) {
+					tempAreaHtml += '<option data-id="' + item.areaId + '">'
+							+ item.areaName + '</option>';
+				});
+				$('#shop-category').html(tempHtml);
+				$('#area').html(tempAreaHtml);
+			}
+		});
+		// 点击提交的时候，获取表单的内容
+		$('#submit').click(
+				function() {
+					var shop = {};
+					shop.shopName = $('#shop-name').val();
+					shop.shopAddr = $('#shop-addr').val();
+					shop.phone = $('#shop-phone').val();
+					shop.shopDesc = $('#shop-desc').val();
+					shop.shopCategoty = {
+						shopCategoryId : $('#shop-category').find('option')
+								.not(function() {
+									return !this.selected;
+								}).data('id')
+					};
+					shop.area = {
+						areaId : $('#area').find('option').not(function() {
+							return !this.selected;
+						}).data('id')
+					};
+					var shopImg = $('#shop-img')[0].file[0];
+					var formData = new fomData();
+					formData.append('shopImg', shopImg);
+					formData.append('shopStr', JSON.stringify(shop));
+					$.ajax({
+						url : registerShopUrl,
+						type : 'POST',
+						data : formData,
+						// 既要传文件，又要传文字
+						contentType : false,
+						proceesData : false,
+						cache : false,
+						success : function(data) {
+							if (data.success) {
+								$.toast('提交成功');
+							} else {
+								$.toast('提交失败' + data.errMsg);
+							}
+						}
+					});
+				});
+	}
+})
