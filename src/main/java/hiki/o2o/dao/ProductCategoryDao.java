b43f2810e87a6c1a6863fd3b11aa8b2bcb552f80
Hikiy <414950815@qq.com>
2018-11-07 15:31:47 +0800

    编写删除商品类别Dao层，并进行单元测试，修复bug

--- src/main/java/hiki/o2o/dao/ProductCategoryDao.java
+++ src/main/java/hiki/o2o/dao/ProductCategoryDao.java
@@ -5,6 +5,8 @@ package hiki.o2o.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import hiki.o2o.entity.ProductCategory;
 
 /**
@@ -19,11 +21,20 @@ public interface ProductCategoryDao {
 	 * @return
 	 */
 	List<ProductCategory> queryProdunctCategoryList(long shopId);
-	
+
 	/**
 	 * 批量新增商品类别
+	 * 
 	 * @param productCategoryList
 	 * @return
 	 */
 	int batchInsertProductCategory(List<ProductCategory> productCategoryList);
+
+	/**
+	 * 删除指定商品类别
+	 * 
+	 * @param productCategoryId
+	 * @return
+	 */
+	int deleteProductCategory(@Param("productCategoryId") long productCategoryId, @Param("shopId") long shopId);
 }
--- src/test/java/hiki/o2o/dao/ProductCategoryDaoTest.java
+++ src/test/java/hiki/o2o/dao/ProductCategoryDaoTest.java
@@ -9,8 +9,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.junit.Ignore;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import hiki.o2o.BaseTest;
@@ -20,32 +21,45 @@ import hiki.o2o.entity.ProductCategory;
  * @Hiki msi
  *
  */
-public class ProductCategoryDaoTest extends BaseTest{
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ProductCategoryDaoTest extends BaseTest {
 	@Autowired
 	private ProductCategoryDao productCategoryDao;
 
 	@Test
-	public void testBatchInserProductCategory(){
-		ProductCategory productCategory1=new ProductCategory();
+	public void testABatchInserProductCategory() {
+		ProductCategory productCategory1 = new ProductCategory();
 		productCategory1.setProductCategoryName("商品类别1");
 		productCategory1.setPriority(1);
 		productCategory1.setCreateTime(new Date());
 		productCategory1.setShopId(2L);
-		ProductCategory productCategory2=new ProductCategory();
+		ProductCategory productCategory2 = new ProductCategory();
 		productCategory2.setProductCategoryName("商品类别2");
 		productCategory2.setPriority(2);
 		productCategory2.setCreateTime(new Date());
 		productCategory2.setShopId(2L);
-		List<ProductCategory> productCategoryList=new ArrayList<ProductCategory>();
+		List<ProductCategory> productCategoryList = new ArrayList<ProductCategory>();
 		productCategoryList.add(productCategory1);
 		productCategoryList.add(productCategory2);
-		int effectedNum=productCategoryDao.batchInsertProductCategory(productCategoryList);
-		assertEquals(2,effectedNum);
+		int effectedNum = productCategoryDao.batchInsertProductCategory(productCategoryList);
+		assertEquals(2, effectedNum);
 	}
-	
+
 	@Test
-	@Ignore
-	public void testQueryByShopId() {
+	public void testCDeleteProductCategory() throws Exception {
+		long shopId = 2;
+		List<ProductCategory> productCategoryList = productCategoryDao.queryProdunctCategoryList(shopId);
+		for (ProductCategory pc : productCategoryList) {
+			if (pc.getProductCategoryName().equals("商品类别1")  || pc.getProductCategoryName().equals("商品类别2")) {
+				System.out.println(pc.getProductCategoryName());
+				int effectedNum = productCategoryDao.deleteProductCategory(pc.getProductCategoryId(), shopId);
+				assertEquals(1, effectedNum);
+			}
+		}
+	}
+
+	@Test
+	public void testBQueryByShopId() {
 		List<ProductCategory> productCategoryList = productCategoryDao.queryProdunctCategoryList(2L);
 		System.out.println("该店铺自定义类别数为：" + productCategoryList.size());
 	}
