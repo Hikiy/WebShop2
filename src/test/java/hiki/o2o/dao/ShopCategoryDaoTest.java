cca88af749d8742d93b84510e65e76b40bca7a43
Hikiy <414950815@qq.com>
2018-12-03 10:47:18 +0800

    修改店铺类别查询功能（增加一级类别查询）的Dao层并单元测试成功

--- src/test/java/hiki/o2o/dao/ShopCategoryDaoTest.java
+++ src/test/java/hiki/o2o/dao/ShopCategoryDaoTest.java
@@ -23,15 +23,10 @@ public class ShopCategoryDaoTest extends BaseTest {
 
 	@Test
 	public void testQueryShopCategory() {
-		List<ShopCategory> shopCategoryList = shopCategoryDao.queryShopCategory(new ShopCategory());
-		assertEquals(2, shopCategoryList.size());
-		ShopCategory testCategory = new ShopCategory();
-		ShopCategory parentCategory = new ShopCategory();
-		parentCategory.setShopCategoryId(1L);
-		testCategory.setParent(parentCategory);
-		shopCategoryList = shopCategoryDao.queryShopCategory(testCategory);
-		assertEquals(1, shopCategoryList.size());
-		System.out.println(shopCategoryList.get(0).getShopCategoryName());
+		List<ShopCategory> shopCategoryList1 = shopCategoryDao.queryShopCategory(null);
+		assertEquals(1,shopCategoryList1.size());
+		List<ShopCategory> shopCategoryList2 = shopCategoryDao.queryShopCategory(new ShopCategory());
+		assertEquals(1,shopCategoryList2.size());
 	}
 
 }
